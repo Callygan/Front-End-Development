c762de7e31c3765e8f3a27547afafc4ad0fa279c
Callygan <podar.dumitru@bk.ru>
2025-12-24 19:41:02 +0200

    Task 2.8: Create an online store

--- JS/Task_2_8/js/data.js
+++ JS/Task_2_8/js/data.js
@@ -1,16 +1,16 @@
 export const products = [
-    {
-        id: 1, 
-        title: "Stylish chair",
-        price: 50.00,
-        category: "Fox kids",
-        image: "assets/images/fox1.png"
-    },
-    {
-        id: 2,
-        title: "Stylish chair",
-        price: 70.00,
-        category: "Forest",
-        image: "assets/images/fox2.png"
-    }
+  {
+    id: 1,
+    title: "Stylish chair",
+    price: 50,
+    category: "Forest",
+    image: "assets/images/fox1.png"
+  },
+  {
+    id: 2,
+    title: "Stylish 2",
+    price: 70,
+    category: "Fox kids",
+    image: "assets/images/fox2.png"
+  }
 ];
